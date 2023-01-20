b0f01ac3d1c12dbfe0d9e5390e581080a31b0027
aaronjan98 <aaronjan98@gmail.com>
2023-01-20 12:55:27 -0800

    WIP create network handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+// import { Route, Link, Switch } from "react-router-dom";
+import { Container } from 'react-bootstrap'
 import { ethers } from 'ethers'
-
-import NFT_ABI from '../abis/NFT.json'
-import config from '../config.json'
 
 import Navigation from './Navigation'
 import Loading from './Loading'
+
+import NFT_ABI from '../abis/NFT.json'
+import config from '../config.json'
 
 function App() {
   const [provider, setProvider] = useState(null)
   const [nft, setNFT] = useState(null)
 
+  const [chainId, setChainId] = useState(null)
   const [account, setAccount] = useState(null)
   const [balance, setBalance] = useState(0)
   const [wallet, setWallet] = useState(null)
@@ -24,6 +26,21 @@ function App() {
     setProvider(provider)
 
     const { chainId } = await provider.getNetwork()
+    setChainId(chainId)
+
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account from Metamask when changed
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
 
     // Initiate contract
     const nft = new ethers.Contract(
@@ -33,18 +50,13 @@ function App() {
     )
     setNFT(nft)
 
-    // Fetch accounts
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
-
     // Fetch all NFTs from account wallet
     setWallet(await nft.walletOfOwner(account))
 
     // Fetch account balance
-    setBalance(await nft.balanceOf(account))
+    let balance = await provider.getBalance(account)
+    balance = ethers.utils.formatUnits(balance, 18)
+    setBalance(balance)
 
     setIsLoading(false)
   }
@@ -55,7 +67,26 @@ function App() {
     }
   }, [isLoading])
 
-  return <div>Hello World</div>
+  return (
+    <Container>
+      <Navigation account={account} chainId={chainId} />
+
+      <hr />
+
+      <h1 className="my-4 text-center">React Hardhat Template</h1>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <p className="text-center">
+            <strong>Your ETH Balance:</strong> {balance} ETH
+          </p>
+          <p className="text-center">Edit App.js to add your code here.</p>
+        </>
+      )}
+    </Container>
+  )
 }
 
 export default App
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,56 @@
-import Navbar from "react-bootstrap/Navbar";
+import { ethers } from 'ethers'
+import { Nav } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Blockies from 'react-blockies'
 
-import logo from "../logo.svg";
+import logo from '../logo.svg'
 
-const Navigation = ({ account }) => {
+import config from '../config.json'
+const { ethereum } = window
+
+const Navigation = ({ account, chainId }) => {
+  const connectHandler = async () => {
+    const accounts = await ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+    const account = ethers.utils.getAddress(accounts[0])
+  }
+
+  const networkHandler = async e => {
+    try {
+      console.log(e.target.value)
+      // Switch to the selected network
+      await ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: e.target.value }],
+      })
+    } catch (switchError) {
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (switchError.code === 4902) {
+        try {
+          await ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: e.target.value,
+                rpcUrls: ['http://127.0.0.1:9002'],
+              },
+            ],
+          })
+        } catch (error) {
+          console.error("AJ's Error: failed to add chain to MetaMask", error)
+        }
+      } else {
+        console.log(
+          `AJ - ${switchError.code}: failed to switch networks `,
+          switchError
+        )
+      }
+    }
+  }
+
   return (
     <Navbar className="my-3">
       <img
@@ -12,12 +60,48 @@ const Navigation = ({ account }) => {
         height="40"
         className="d-inline-block align-top mx-3"
       />
-      <Navbar.Brand href="#">Dapp Punks</Navbar.Brand>
+      <Navbar.Brand href="#">Text2Image NFT Minter</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>{account}</Navbar.Text>
+        <Nav className="mr-auto">
+          <Nav.Link href="#generate">Generate Text-to-Image Art</Nav.Link>
+          <Nav.Link href="#mint">Mint NFT</Nav.Link>
+          <Nav.Link href="#view">View My NFTs</Nav.Link>
+        </Nav>
+
+        <div className="d-flex justify-content-end mt-3">
+          <Form.Select
+            aria-label="Network Selector"
+            value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
+            onChange={networkHandler}
+            style={{ maxWidth: '200px', marginRight: '20px' }}
+          >
+            <option value="0" disabled>
+              Select Network
+            </option>
+            <option value={`0x${(1338).toString(16)}`}>Localhost</option>
+            <option value="0x5">Goerli</option>
+          </Form.Select>
+
+          {account ? (
+            <Navbar.Text className="d-flex align-items-center">
+              {account.slice(0, 5) + '...' + account.slice(38, 42)}
+              <Blockies
+                seed={account}
+                size={10}
+                scale={3}
+                color="#2187D0"
+                bgColor="#F1F2F9"
+                spotColor="#767F92"
+                className="identicon mx-2"
+              />
+            </Navbar.Text>
+          ) : (
+            <Button onClick={connectHandler}>Connect</Button>
+          )}
+        </div>
       </Navbar.Collapse>
     </Navbar>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
