c96679769eb65b7e7e23dad5a517db0f2a43a548
aaronjan98 <aaronjan98@gmail.com>
2023-01-20 23:56:58 -0800

    WIP create menu dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import { ethers } from 'ethers'
-import { Nav } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap'
 import Blockies from 'react-blockies'
 
+import '../styles/hamburger.css'
 import logo from '../logo.svg'
 
 import config from '../config.json'
 const { ethereum } = window
 
 const Navigation = ({ account, setAccount, chainId }) => {
+  const [isActive, setIsActive] = useState(false)
+
   const connectHandler = async () => {
     const accounts = await ethereum.request({
       method: 'eth_requestAccounts',
@@ -51,7 +59,41 @@ const Navigation = ({ account, setAccount, chainId }) => {
   }
 
   return (
-    <Navbar className="my-3">
+    <Navbar className="mb-3 mt-4">
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle
+          id="basic-navbar-nav"
+          aria-controls="basic-navbar-nav"
+          aria-label="Toggle navigation"
+          className={`ham ${isActive ? 'active' : ''} custom-toggle my-navbar`}
+          onClick={() => setIsActive(!isActive)}
+        >
+          <svg
+            class="ham hamRotate ham8"
+            viewBox="0 0 100 100"
+            width="80"
+            onclick="this.classList.toggle('active')"
+          >
+            <path
+              class="line top"
+              d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+            />
+            <path class="line middle" d="m 30,50 h 40" />
+            <path
+              class="line bottom"
+              d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+            />
+          </svg>
+        </Navbar.Toggle>
+        <Nav className="mr-auto">
+          <NavDropdown.Item href="#generate">
+            Generate Text-to-Image Art
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#mint">Mint NFT</NavDropdown.Item>
+          <NavDropdown.Item href="#view">View My NFTs</NavDropdown.Item>
+        </Nav>
+      </Navbar.Collapse>
+
       <img
         alt="logo"
         src={logo}
@@ -61,18 +103,19 @@ const Navigation = ({ account, setAccount, chainId }) => {
       />
       <Navbar.Brand href="#">Text2Image NFT Minter</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Nav className="mr-auto">
-          <Nav.Link href="#generate">Generate Text-to-Image Art</Nav.Link>
-          <Nav.Link href="#mint">Mint NFT</Nav.Link>
-          <Nav.Link href="#view">View My NFTs</Nav.Link>
-        </Nav>
-
-        <div className="d-flex justify-content-end mt-3">
+        <div
+          className="d-flex justify-content-end"
+          style={{ alignItems: 'center' }}
+        >
           <Form.Select
             aria-label="Network Selector"
             value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
             onChange={networkHandler}
-            style={{ maxWidth: '200px', marginRight: '20px' }}
+            style={{
+              maxWidth: '200px',
+              marginRight: '20px',
+              height: '100%',
+            }}
           >
             <option value="0" disabled>
               Select Network
