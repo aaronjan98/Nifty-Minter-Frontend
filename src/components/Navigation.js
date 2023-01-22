fd7d0e6f894bb0d2996d8b837f8c027c1bfbfc90
aaronjan98 <aaronjan98@gmail.com>
2023-01-21 16:04:55 -0800

    create hamburger menu dropdow

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ import {
 } from 'react-bootstrap'
 import Blockies from 'react-blockies'
 
-import '../styles/hamburger.css'
+import '../styles/hamburger.scss'
 import logo from '../logo.svg'
 
 import config from '../config.json'
@@ -60,88 +60,80 @@ const Navigation = ({ account, setAccount, chainId }) => {
 
   return (
     <Navbar className="mb-3 mt-4">
-      <Navbar.Collapse id="basic-navbar-nav">
+      <div
+        className="d-flex justify-content-start"
+        style={{ alignItems: 'center' }}
+      >
         <Navbar.Toggle
-          id="basic-navbar-nav"
-          aria-controls="basic-navbar-nav"
-          aria-label="Toggle navigation"
-          className={`ham ${isActive ? 'active' : ''} custom-toggle my-navbar`}
+          className={`ham ${isActive ? 'active' : ''} custom-toggle`}
           onClick={() => setIsActive(!isActive)}
         >
-          <svg
-            class="ham hamRotate ham8"
-            viewBox="0 0 100 100"
-            width="80"
-            onclick="this.classList.toggle('active')"
-          >
-            <path
-              class="line top"
-              d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
-            />
-            <path class="line middle" d="m 30,50 h 40" />
-            <path
-              class="line bottom"
-              d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
-            />
-          </svg>
+          <div className="menu-icon">
+            <input className="menu-icon__cheeckbox" type="checkbox" />
+            <div>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <NavDropdown show={isActive}>
+            <NavDropdown.Item href="#generate">
+              Generate Text-to-Image Art
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#mint">Mint NFT</NavDropdown.Item>
+            <NavDropdown.Item href="#view">View My NFTs</NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Toggle>
-        <Nav className="mr-auto">
-          <NavDropdown.Item href="#generate">
-            Generate Text-to-Image Art
-          </NavDropdown.Item>
-          <NavDropdown.Item href="#mint">Mint NFT</NavDropdown.Item>
-          <NavDropdown.Item href="#view">View My NFTs</NavDropdown.Item>
-        </Nav>
-      </Navbar.Collapse>
 
-      <img
-        alt="logo"
-        src={logo}
-        width="40"
-        height="40"
-        className="d-inline-block align-top mx-3"
-      />
-      <Navbar.Brand href="#">Text2Image NFT Minter</Navbar.Brand>
-      <Navbar.Collapse className="justify-content-end">
-        <div
-          className="d-flex justify-content-end"
-          style={{ alignItems: 'center' }}
-        >
-          <Form.Select
-            aria-label="Network Selector"
-            value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
-            onChange={networkHandler}
-            style={{
-              maxWidth: '200px',
-              marginRight: '20px',
-              height: '100%',
-            }}
-          >
-            <option value="0" disabled>
-              Select Network
-            </option>
-            <option value={`0x${(1338).toString(16)}`}>Localhost</option>
-            <option value="0x5">Goerli</option>
-          </Form.Select>
-
-          {account ? (
-            <Navbar.Text className="d-flex align-items-center">
-              {account.slice(0, 5) + '...' + account.slice(38, 42)}
-              <Blockies
-                seed={account}
-                size={10}
-                scale={3}
-                color="#2187D0"
-                bgColor="#F1F2F9"
-                spotColor="#767F92"
-                className="identicon mx-2"
-              />
-            </Navbar.Text>
-          ) : (
-            <Button onClick={connectHandler}>Connect</Button>
-          )}
+        <div className="d-flex justify-content-center">
+          <img
+            alt="logo"
+            src={logo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top mx-3"
+          />
+          <Navbar.Brand href="#">Text2Image NFT Minter</Navbar.Brand>
         </div>
-      </Navbar.Collapse>
+      </div>
+
+      <div
+        className="d-flex justify-content-end"
+        style={{ alignItems: 'center' }}
+      >
+        <Form.Select
+          aria-label="Network Selector"
+          value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
+          onChange={networkHandler}
+          style={{
+            maxWidth: '200px',
+            marginRight: '20px',
+            height: '100%',
+          }}
+        >
+          <option value="0" disabled>
+            Select Network
+          </option>
+          <option value={`0x${(1338).toString(16)}`}>Localhost</option>
+          <option value="0x5">Goerli</option>
+        </Form.Select>
+
+        {account ? (
+          <Navbar.Text className="d-flex align-items-center">
+            {account.slice(0, 5) + '...' + account.slice(38, 42)}
+            <Blockies
+              seed={account}
+              size={10}
+              scale={3}
+              color="#2187D0"
+              bgColor="#F1F2F9"
+              spotColor="#767F92"
+              className="identicon mx-2"
+            />
+          </Navbar.Text>
+        ) : (
+          <Button onClick={connectHandler}>Connect</Button>
+        )}
+      </div>
     </Navbar>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
